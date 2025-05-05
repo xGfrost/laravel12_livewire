478f73dea6fde3c5ded4cc6dc167d032f9ba3545
xGfrost <fadelakbar528@gmail.com>
2025-05-05 15:05:46 +0700

    create, edit, index contracts already fix

--- app/Livewire/Admin/Contracts/Create.php
+++ app/Livewire/Admin/Contracts/Create.php
@@ -2,13 +2,60 @@
 
 namespace App\Livewire\Admin\Contracts;
 
+use App\Models\Contract;
 use App\Models\Department;
+use App\Models\Employee;
 use Livewire\Component;
 
 class Create extends Component
 {
+    public $contract;
+
+    public $search = '';
+
+    public $departments_id;
+
+    public function rules()
+    {
+        return [
+            'contract.designation_id' => 'required',
+            'contract.employee_id' => 'required',
+            'contract.start_date' => 'required|date',
+            'contract.end_date' => 'required|date|after:contract.start_date',
+            'contract.rate_type' => 'required',
+            'contract.rate' => 'required|numeric',
+        ];
+    }
+
+    public function mount()
+    {
+        $this->contract = new Contract();
+    }
+
+    public function selectEmployee($id)
+    {
+        $this->contract->employee_id = $id;
+        $this->search = $this->contract->employee->name;
+    }
+
+    public function save()
+    {
+        $this->validate();
+        // $this->contract->company_id = session('company_id');
+        $this->contract->save();
+        session()->flash('success', 'Contract created successfully.');
+        return $this->redirectIntended(route('contracts.index'));
+    }
+
     public function render()
     {
-        return view('livewire.admin.contracts.create');
+        $employees = Employee::inCompany()->searchByName($this->search)->get();
+        $departments = Department::inCompany()->get();
+        $designations = $this->departments_id ? Department::find($this->departments_id)->designations : collect();
+        return view('livewire.admin.contracts.create',[
+            'employees' => $employees,
+            'departments' => $departments,
+            'designations' => $designations,
+        ]);
     }
 }
--- app/Livewire/Admin/Contracts/Index.php
+++ app/Livewire/Admin/Contracts/Index.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Admin\Contracts;
 
+use App\Models\Contract;
 use App\Models\Department;
 use Livewire\Component;
 use Livewire\WithoutUrlPagination;
@@ -9,9 +10,19 @@ use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination, WithoutUrlPagination;
 
+    public $search = '';
+
+    public function delete($id)
+    {
+        Contract::find($id)->delete();
+        session()->flash('success', 'Contract deleted successfully.');
+    }
     public function render()
     {
-        return view('livewire.admin.contracts.index');
+        return view('livewire.admin.contracts.index', [
+            'contracts' => Contract::inCompany()->searchByEmployee($this->search)->paginate(5),
+        ]);
     }
 }
