d255544a8c081160d690153cb80535f706f8624f
xGfrost <fadelakbar528@gmail.com>
2025-05-05 10:57:31 +0700

    index, show payroll already have function

--- app/Livewire/Admin/Payrolls/Index.php
+++ app/Livewire/Admin/Payrolls/Index.php
@@ -2,12 +2,84 @@
 
 namespace App\Livewire\Admin\Payrolls;
 
+use App\Models\Employee;
+use App\Models\Payroll;
+use Carbon\Carbon;
+use Illuminate\Validation\ValidationException;
 use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination, WithoutUrlPagination;
+
+    public $monthYear;
+
+    public function rules()
+    {
+        return[
+            'monthYear' => 'required',
+        ];
+    }
+
+    public function generatePayroll()
+    {
+        $this->validate();
+        $date = Carbon::parse($this->monthYear);
+        if(Payroll::inCompany()->where('month_year', $date->format('Y-m'))->exists()){
+            throw ValidationException::withMessages([
+                'monthYear' => 'Payroll for this month has already been generated.',
+            ]);
+        }else{
+            $payroll = new Payroll();
+            $payroll->month = $date->format('m');
+            $payroll->year = $date->format('Y');
+            $payroll->company_id = session('company_id');
+            $payroll->save();
+            foreach(Employee::inCompany()->get() as $employee){
+                $contract = $employee->getActiveContract($date->startOfMonth()->toDateString(), $date->endOfMonth()->toDateString());
+                if($contract){
+                    $employee->basic_salary = $contract->basic_salary;
+                $payroll->salaries()->create(
+                    [
+                        'employee_id' => $employee->id,
+                        'gross_salary' => $contract->getTotalEarnings($date->format('Y-m')),
+                    ]
+                );
+            }else{
+                $payroll->employees()->attach($employee->id, ['basic_salary' => 0]);
+            }
+        }
+        session()->flash('success', 'Payroll updated successfully.');
+    }
+
+}
+
+    public function updatePayroll($id)
+    {
+        $payroll = Payroll::inCompany()->find($id);
+        $payroll->salaries->delete();
+        foreach(Employee::inCompany()->get() as $employee){
+            $contract = $employee->getActiveContract($payroll->start_date, $payroll->end_date);
+            if($contract){
+                $employee->basic_salary = $contract->basic_salary;
+                $payroll->salaries()->create(
+                    [
+                        'employee_id' => $employee->id,
+                        'gross_salary' => $contract->getTotalEarnings($payroll->month_year),
+                    ]
+                );
+            }else{
+                $payroll->employees()->attach($employee->id, ['basic_salary' => 0]);
+            }
+        }
+        session()->flash('success', 'Payroll updated successfully.');
+    }
     public function render()
     {
-        return view('livewire.admin.payrolls.index');
+        return view('livewire.admin.payrolls.index', [
+            'payrolls' => Payroll::inCompany()->orderBy('year', 'desc')->orderBy('month', 'desc')->paginate(5),
+        ]);
     }
 }
--- app/Livewire/Admin/Payrolls/Show.php
+++ app/Livewire/Admin/Payrolls/Show.php
@@ -2,10 +2,31 @@
 
 namespace App\Livewire\Admin\Payrolls;
 
+use App\Models\Payroll;
+use App\Models\Salary;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class Show extends Component
 {
+    public $payroll;
+
+    public function mount($id)
+    {
+        $this->payroll = Payroll::inCompany()-> find($id);
+    }
+
+    public function generatePayslip($id)
+    {
+        $salary = Salary::find($id);
+        $pdf = Pdf::loadView('pdf.payslip', ['salary' => $salary]);
+        $pdf->setPaper(array(0, 0, 400, 1500), 'potrait');
+        $filepath = storage_path(Str::slug($salary->employee->name) . '-payslip.pdf');
+        $pdf->save($filepath);
+        return response()->download($filepath)->deleteFileAfterSend(true);
+
+    }
     public function render()
     {
         return view('livewire.admin.payrolls.show');
